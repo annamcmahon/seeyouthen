1be2f47fbdc402bc24fae4cb7d57ca6874a37132
amcmaho4 <amcmaho4@nd.edu>
2015-09-12 15:09:10 -0400

    picker view communicating w/ parse

--- See You Then/TimerViewController.swift
+++ See You Then/TimerViewController.swift
@@ -13,8 +13,12 @@ import Parse
 class TimerViewController: UIViewController,UIPickerViewDelegate {
     
     @IBOutlet weak var timePicker: UIPickerView!
+	var currentPerson : PFUser = PFUser()
+
+	@IBOutlet var personName: UILabel!
+	let timePickerHoursData = [0,1, 2, 3, 4, 5]//["0", "1", "2", "3", "4", "5"]
     
-    let timePickerData = ["5","10","15"]
+    let timePickerMinutesData = [00,05,10, 15,20, 30]//["00", "05","10","15", "20", "30"]
     
     
     override func viewDidLoad() {
@@ -23,6 +27,7 @@ class TimerViewController: UIViewController,UIPickerViewDelegate {
         timePicker.delegate = self
         // Do any additional setup after loading the view.
         // get user picker data
+		personName.text = currentPerson.username
 		
     }
 
@@ -31,21 +36,52 @@ class TimerViewController: UIViewController,UIPickerViewDelegate {
         // Dispose of any resources that can be recreated.
     }
 	
-    
-    //func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-       // return 1
-    //}
+    // components are ACROSS
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+	// Scrollable upt and down
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-			return timePickerData.count
+			if(component == 0){
+				return timePickerHoursData.count
+			}
+			else{
+				return timePickerMinutesData.count
+			}
 		}
 	
 	func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
-			return self.timePickerData[row]
+		if(component == 0){
+			return String(self.timePickerHoursData[row])
+		}
+		else{
+			return String(timePickerMinutesData[row])
+		}
 		}
 		
     @IBAction func startButtonClicked(sender: UIButton) {
-        
+		
+		var hours = timePickerHoursData[timePicker.selectedRowInComponent(0)]
+		var minutes = timePickerMinutesData[timePicker.selectedRowInComponent(1)]
+		var timeMinutes = minutes + hours*60;
+		
         self.title = "Stop";
+		
+		var challenge = PFObject(className:"Challenges")
+		challenge.setValue("ANNA", forKey: "Challenger")
+		challenge.setValue(currentPerson.username!, forKey: "Challenged")
+		challenge.setValue(timeMinutes, forKey: "timeLeft")
+		
+		
+		challenge.saveInBackgroundWithBlock {
+			(success: Bool, error: NSError?) -> Void in
+			if (success) {
+			println("success")
+			} else {
+				println("not")
+
+			}
+		}
     }
 
 }
--- See You Then/UsersTableViewController.swift
+++ See You Then/UsersTableViewController.swift
@@ -11,7 +11,8 @@ import Parse
 
 class UsersTableViewController: UITableViewController {
     
-    var userData: Array<AnyObject>!
+    var userData = Array<AnyObject>()
+	var thePersonSelected = 0;
 
     
     func getParseData() -> Array<AnyObject>{
@@ -23,20 +24,29 @@ class UsersTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        /*var query = PFUser.query()!
-        query.whereKey("username", equalTo: "AnnaM")
-        userData = query.findObjects()
-        */
-        /*var query = PFQuery(className:"Challenges")
-        query.getObjectInBackgroundWithId("Fu2NG2AO7a") {
-             (gameScore: PFObject?, error: NSError?) -> Void in
-             if error == nil && gameScore != nil {
-               println(gameScore)
-             } else {
-               println(error)
-             }*/
-    
+        var query = PFUser.query()!
+        //query.whereKey("username", equalTo: "AnnaM")
+        userData = query.findObjects()!
+
+//		query = PFQuery(className:"Challenges")
+//        query.getObjectInBackgroundWithId("Fu2NG2AO7a") {
+//		 (gameScore: PFObject?, error: NSError?) -> Void in
+//             if error == nil && gameScore != nil {
+//               //println(gameScore)
+//             } else {
+//             //  println(error)
+//             }
+		
     }
+	
+	override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+		let cell = tableView.cellForRowAtIndexPath(indexPath)
+		
+		if cell != nil {
+			thePersonSelected = indexPath.row
+			self.performSegueWithIdentifier("personSelected", sender: self)
+		}
+	}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -48,7 +58,7 @@ class UsersTableViewController: UITableViewController {
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -61,11 +71,14 @@ class UsersTableViewController: UITableViewController {
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		
         if let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as? UserTableViewCell {
             var row = indexPath.row
+			
         
         cell.userName.text = userData[row].username
-        //cell.status = userData[row].status
+		cell.status.text =  "1"//userData[row].attribute("status", atIndex: row, effectiveRange: 1,10)
+
         //cell.profPic = userData[row].profPic
         return cell
         }
@@ -74,6 +87,13 @@ class UsersTableViewController: UITableViewController {
 
         return (tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as? UITableViewCell)!
     }
+	
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		if segue.identifier == "personSelected" {
+			let nextView = segue.destinationViewController as! TimerViewController
+			nextView.currentPerson = userData[thePersonSelected] as! PFUser
+		}
+	}
 
     /*
     // Override to support conditional editing of the table view.
