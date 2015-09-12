69efaf6ed4044d2baa532f02845689bcb5d1cb02
amcmaho4 <amcmaho4@nd.edu>
2015-09-12 05:01:03 -0400

    login works

---
+++ See You Then/SignUpController.swift
@@ -0,0 +1,63 @@
+//
+//  SignUpController.swift
+//  See You Then
+//
+//  Created by Anna Jo McMahon on 9/12/15.
+//  Copyright (c) 2015 anna. All rights reserved.
+//
+import Parse
+import UIKit
+
+
+
+
+class SignUpController: UIViewController {
+	@IBOutlet var password: UITextField!
+	@IBOutlet var email: UITextField!
+	@IBOutlet var username: UITextField!
+	
+	@IBAction func signupAction(sender: UIButton) {
+		var user = PFUser()
+  		user.username = username.text
+  		user.password = password.text
+  		user.email = email.text
+  // other fields can be set just like with PFObject
+  //user["phone"] = "415-392-0202"
+		
+  user.signUpInBackgroundWithBlock {
+	(succeeded: Bool, error: NSError?) -> Void in
+	if let error = error {
+		let errorString = error.userInfo?["error"] as? NSString
+		self.showAlert(errorString!)
+	} else {
+		self.performSegueWithIdentifier("signupSegue", sender: self)
+	}
+  }
+		
+	}
+	
+	
+	func showAlert(error: NSString){
+		var createAccountErrorAlert: UIAlertView = UIAlertView()
+		createAccountErrorAlert.delegate = self
+		createAccountErrorAlert.title = "Oops"
+		createAccountErrorAlert.message = error as String
+		createAccountErrorAlert.addButtonWithTitle("OK")
+		createAccountErrorAlert.show()
+	}
+	
+	func alertView(View: UIAlertView!, clickedButtonAtIndex buttonIndex: Int){
+		switch buttonIndex{
+		case 0:
+			NSLog("Retry");
+			break;
+		default:
+			NSLog("Default");
+			break;
+			//Some code here..
+			
+		}
+	}
+	
+
+}
---
+++ See You Then/logincontroller.swift
@@ -0,0 +1,87 @@
+//
+//  loginViewController.swift
+//  wellness
+//
+//  Created by Anna Jo McMahon on 3/2/15.
+//  Copyright (c) 2015 Andrei Puni. All rights reserved.
+//
+import Parse
+import UIKit
+
+class loginViewController: UIViewController {
+	
+	@IBOutlet var usernametext: UITextField!
+	@IBOutlet var passwordtext: UITextField!
+	@IBOutlet var loginButton: UIButton!
+	@IBOutlet var createAccount: UIButton!
+	@IBOutlet var backView: UIView!
+
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		//Looks for single or multiple taps.
+		var tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "DismissKeyboard")
+		//backView.addGestureRecognizer(tap)
+		
+	}
+	
+
+	//Calls this function when the tap is recognized.
+	func DismissKeyboard(){
+		//Causes the view (or one of its embedded text fields) to resign the first responder status.
+	//	backView.endEditing(true)
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+
+
+	
+	@IBAction func loginButtonAction(sender: UIButton) {
+		PFUser.logInWithUsernameInBackground(usernametext.text, password:passwordtext.text) {
+			(user: PFUser?, error: NSError?) -> Void in
+			if user != nil {
+				self.performSegueWithIdentifier("completedLoginSegue", sender: self)
+			} else {
+    				self.showAlert()
+			}
+		}
+	
+	}
+	
+	
+	func showAlert(){
+		var createAccountErrorAlert: UIAlertView = UIAlertView()
+		createAccountErrorAlert.delegate = self
+		createAccountErrorAlert.title = "Oops"
+		createAccountErrorAlert.message = "That is not an account"
+		createAccountErrorAlert.addButtonWithTitle("Create Account")
+		createAccountErrorAlert.addButtonWithTitle("Dismiss")
+		createAccountErrorAlert.show()
+	}
+	
+	
+	
+	func alertView(View: UIAlertView!, clickedButtonAtIndex buttonIndex: Int){
+		
+		switch buttonIndex{
+		case 1:
+			NSLog("Retry");
+			break;
+		case 0:
+			self.performSegueWithIdentifier("createAccountSegue", sender: self)
+			break;
+		default:
+			NSLog("Default");
+			break;
+			//Some code here..
+			
+		}
+	}
+	
+	
+}
+
