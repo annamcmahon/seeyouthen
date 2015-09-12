86a66d9e3dc5414b7c70b9882952274ae39d19f3
amcmaho4 <amcmaho4@nd.edu>
2015-09-12 02:22:13 -0400

    added the start button

--- See You Then/ViewController.swift
+++ See You Then/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	@IBAction func startButtonClick(sender: UIButton) {
+		var phoneNumber1 = 720-440-4892
+		var phoneNumber2 = 612-618-3669
+		var time = 10;
+		
+		// make calls to twilio api with two numbers
+	}
 
 }
 
