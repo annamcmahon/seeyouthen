cb1b94f042b13075fd8d52752e9bbcbcdfaa31f6
amcmaho4 <amcmaho4@nd.edu>
2015-09-12 02:02:18 -0400

    Initial Commit

---
+++ See You Then/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  See You Then
+//
+//  Created by Anna Jo McMahon on 9/12/15.
+//  Copyright (c) 2015 anna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
